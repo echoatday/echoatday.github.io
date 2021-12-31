f4de1317524e657f1e3616cfb08884140cb83e5e
echo storrs <echoatday@gmail.com>
2021-12-31 18:46:20 -0500

    Fixed a window alignment issue and added 'projects' scroll link.

--- src/page-About.js
+++ src/page-About.js
@@ -1,9 +1,14 @@
 import './App.scss';
 
+const projScroll = () => {
+  window.scrollTo({top: window.innerHeight, behavior: 'smooth'})
+}
+
 function About() {
   return (
     <div className='About'>
       <div className='Greeting'>HIRE ME</div>
+      <a className='ScrollDown' onClick={projScroll}>projects<br/>▼</a>
     </div>
   );
 }
--- src/page-Projects.js
+++ src/page-Projects.js
@@ -57,37 +57,4 @@ class Portfolio extends React.Component {
   }
 }
 
-
-// pre-api version, export default Projects
-function PortfolioEntry(props) {
-
-  return (
-    <div className='Entry'>
-      <a href={props.link}>arg</a>
-    </div>
-  )
-}
-
-function Projects() {
-  const githubs = ['https://github.com/AcademyPgh/Y5S2-DoorsOpen.Mobile', 'https://github.com/echoatday/75words', 'https://github.com/echoatday/helperbot', 'https://github.com/echoatday/Academy-PGH-Randomizer-13'];
-  const itches = ['https://trashmaker.itch.io/zwitter', 'https://trashmaker.itch.io/picolife', 'https://birdking.itch.io/new-furniture']
-
-  var projectList = [];
-
-  githubs.forEach(element => {
-    projectList.push(<PortfolioEntry link={element} />)
-  })
-
-  itches.forEach(element => {
-    projectList.push(<PortfolioEntry link={element} />)
-  })
-
-  return (
-    <div className='Projects'>
-      {projectList}
-    </div>
-  );
-}
-// pre api version end
-
 export default Portfolio;
